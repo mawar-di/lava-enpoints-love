53a51b4d288605a1c4a7833c7aa54a658246cb6d
mawar_di <135085447+mawar-di@users.noreply.github.com>
2024-05-18 20:21:06 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-f962b4de-3209-45b9-a339-cc643ea35ac1/"
 
 OUTPUT_DIR="logs"
 
